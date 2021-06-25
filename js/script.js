8dc1300752e6c309e31a8f76373efe15feb54fa7
Santi Fernandez <sanfersca@gmail.com>
2021-06-25 13:48:43 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -418,7 +418,7 @@ scene.background = new THREE.Color('black');
 
 {
   const objLoader = new OBJLoader();
-  objLoader.load('assets/leg.obj', (root) => {
+  objLoader.load('assets/foot.obj', (root) => {
     bunny = root;
     scene.add(root);
   });
